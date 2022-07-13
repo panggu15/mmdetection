3c5c3f4141dd087cc25cb4720c5616add58186d7
ultago <83053171+panggu15@users.noreply.github.com>
2022-07-13 21:08:27 +0900

    Update mstrain_3x_coco_instance.py

--- configs/common/mstrain_3x_coco_instance.py
+++ configs/common/mstrain_3x_coco_instance.py
@@ -1,7 +1,7 @@
 _base_ = '../_base_/default_runtime.py'
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = 'data/coco/'
+data_root = 'data/dataset/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 
@@ -46,17 +46,17 @@ data = dict(
         times=3,
         dataset=dict(
             type=dataset_type,
-            ann_file=data_root + 'annotations/instances_train2017.json',
+            ann_file=data_root + "train.json"
             img_prefix=data_root + 'train2017/',
             pipeline=train_pipeline)),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
+        ann_file=ann_file=data_root + "valid.json"
         img_prefix=data_root + 'val2017/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
+        ann_file=data_root + "test.json"
         img_prefix=data_root + 'val2017/',
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric=['bbox', 'segm'])
